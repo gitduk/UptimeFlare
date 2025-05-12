7fb65596556bee75d93460f3880ec65eeda29204
gitduk <wukaigee@gmail.com>
2025-05-12 18:13:06 +0800

    Update uptime.config.ts
    add monitors

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,15 +1,26 @@
-const pageConfig = {
+import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
+
+const pageConfig: PageConfig = {
   // Title for your status page
   title: "wukaige's Status Page",
+  
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://github.com/gitduk', label: 'GitHub' },
     { link: 'https://blog.wukaige.com/', label: 'Blog' },
     { link: 'mailto:wukaigee@gmail.com', label: 'Email Me', highlight: true },
   ],
+  
+  // [OPTIONAL] Group your monitors
+  // If not specified, all monitors will be shown in a single list
+  // If specified, monitors will be grouped and ordered, not-listed monitors will be invisble (but still monitored)
+  // group: {
+  //   '🌐 Public (example group name)': ['foo_monitor', 'bar_monitor', 'more monitor ids...'],
+  //   '🔐 Private': ['test_tcp_monitor'],
+  // },
 }
 
-const workerConfig = {
+const workerConfig: WorkerConfig = {
   // Write KV at most every 3 minutes unless the status changed
   kvWriteCooldownMinutes: 3,
   // Enable HTTP Basic auth for status page & API by uncommenting the line below, format `<USERNAME>:<PASSWORD>`
@@ -51,13 +62,23 @@ const workerConfig = {
       method: 'GET',
       target: 'https://subs.wukaige.com/',
       statusPageLink: 'https://subs.wukaige.com/',
-    },
+    }
   ],
   notification: {
-    appriseApiServer: "https://apprise.example.com/notify",
-    recipientUrl: "tgram://bottoken/ChatID",
-    timeZone: "Asia/Shanghai",
+    // [Optional] apprise API server URL
+    // if not specified, no notification will be sent
+    appriseApiServer: 'https://apprise.example.com/notify',
+    // [Optional] recipient URL for apprise, refer to https://github.com/caronc/apprise
+    // if not specified, no notification will be sent
+    recipientUrl: 'tgram://bottoken/ChatID',
+    // [Optional] timezone used in notification messages, default to "Etc/GMT"
+    timeZone: 'Asia/Shanghai',
+    // [Optional] grace period in minutes before sending a notification
+    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
+    // if not specified, notification will be sent immediately
     gracePeriod: 5,
+    // [Optional] disable notification for monitors with specified ids
+    skipNotificationIds: ['zero'],
   },
   callbacks: {
     onStatusChange: async (
@@ -70,7 +91,6 @@ const workerConfig = {
     ) => {
       // This callback will be called when there's a status change for any monitor
       // Write any Typescript code here
-
       // This will not follow the grace period settings and will be called immediately when the status changes
       // You need to handle the grace period manually if you want to implement it
     },
@@ -87,5 +107,28 @@ const workerConfig = {
   },
 }
 
+// You can define multiple maintenances here
+// During maintenance, an alert will be shown at status page
+// Also, related downtime notifications will be skipped (if any)
+// Of course, you can leave it empty if you don't need this feature
+// const maintenances: MaintenanceConfig[] = []
+const maintenances: MaintenanceConfig[] = [
+  {
+    // [Optional] Monitor IDs to be affected by this maintenance
+    monitors: ['foo_monitor', 'bar_monitor'],
+    // [Optional] default to "Scheduled Maintenance" if not specified
+    title: 'Test Maintenance',
+    // Description of the maintenance, will be shown at status page
+    body: 'This is a test maintenance, server software upgrade',
+    // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
+    start: '2025-04-27T00:00:00+08:00',
+    // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
+    // if not specified, the maintenance will be considered as on-going
+    end: '2025-04-30T00:00:00+08:00',
+    // [Optional] color of the maintenance alert at status page, default to "yellow"
+    color: 'blue',
+  },
+]
+
 // Don't forget this, otherwise compilation fails.
-export { pageConfig, workerConfig }
+export { pageConfig, workerConfig, maintenances }
